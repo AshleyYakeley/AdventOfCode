d63430d20a044126b1b97db1ed572fd93ca2cde6
Ashley Yakeley <ashley@semantic.org>
2021-12-01 17:15:50 -0800

    01

---
+++ app/2021/01/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+import System.IO
+
+solve1 :: [Int] -> Int
+solve1 xx = length $ filter id $ zipWith (>) xx $ maxBound : xx
+
+solve2 :: [Int] -> Int
+solve2 xx = length $ filter id $ zipWith (>) xx $ maxBound : maxBound : maxBound : xx
+
+main :: IO ()
+main = do
+    f <- readFile "app/2021/01/input.txt"
+    let xx = fmap read $ lines f
+    putStrLn $ "Part 1: " <> show (solve1 xx)
+    putStrLn $ "Part 2: " <> show (solve2 xx)
